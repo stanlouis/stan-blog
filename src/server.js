b3c10a95304144dc0dc2cbe53bb10bbf77481cf6
Stanley Louis <stanlouis@msn.com>
2019-09-26 16:36:48 -0400

    add MongoDB to server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-    comments: []
-  },
-  "learn-node": {
-    upvotes: 0,
-    comments: []
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-    comments: []
-  }
-};
+import { MongoClient } from "mongodb";
 
 const app = express();
 
@@ -23,6 +9,26 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true
+    });
+    const db = client.db("my-blog");
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
   const articleName = req.params.name;
