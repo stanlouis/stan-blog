15480eb0701fb31c7059806110f66107cf0a93ef
Stanley Louis <stanlouis@msn.com>
2019-09-26 15:42:27 -0400

    add upvote post route

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+    comments: []
+  },
+  "learn-node": {
+    upvotes: 0,
+    comments: []
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+    comments: []
+  }
+};
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
+    );
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
